730c5ad2c496be4eae0aa8717cb812a1088f279c
SaifAlam <kaif0350@gmail.com>
2025-01-01 15:41:19 +0530

    First Dsa program

---
+++ firstProgram.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+
+
+int main(){
+    cout << "Hello Kaif" << endl << "Hey aditi" << endl;
+    cout << "Hello Saif";
+    
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Enter two integers: ";
+    cin >> x >> y;  
+    cout << "Value of x: " << x << " and y: " << y << endl; 
+    return 0;
+}
